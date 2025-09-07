44eaae78e9bc0d83f201473cc693f4baa8d47083
Mohan Singh <mohanchouhan1777@gmail.com>
2025-09-07 19:36:39 +0530

    day 2

--- ml/ml_algo/linear regression/linear_regresion.py
+++ ml/ml_algo/linear regression/linear_regresion.py
@@ -3,6 +3,7 @@ house_price=[150000, 180000, 210000, 250000, 300000]
 
 
 
+
 def model(x):
     sum_x=0
     sum_y=0
@@ -23,6 +24,41 @@ def model(x):
 
     return slope*x+intercept
 
-x=input("Enter the area of the house in square feet: ")
-x = int(x)
-print(model(x))
+
+
+# reggressopn with multiple features
+
+import numpy as np
+
+x1=np.array([1200, 1500, 1800, 2000, 2500])
+x2=np.array([3, 4, 2, 5, 4])# number of bedrooms
+x3=np.array([3, 2, 4, 3, 5])# number of bathrooms
+y1=np.array([150000, 180000, 210000, 250000, 300000])
+
+def model1(x1, x2, x3):
+    mean_x1 = np.mean(x1)
+    mean_x2 = np.mean(x2)
+    mean_x3 = np.mean(x3)
+    mean_y1 = np.mean(y1)
+
+    numerator1 = np.sum((x1 - mean_x1) * (y1 - mean_y1))
+    denominator1 = np.sum((x1 - mean_x1) ** 2)
+
+    numerator2 = np.sum((x2 - mean_x2) * (y1 - mean_y1))
+    denominator2 = np.sum((x2 - mean_x2) ** 2)
+
+    numerator3 = np.sum((x3 - mean_x3) * (y1 - mean_y1))
+    denominator3 = np.sum((x3 - mean_x3) ** 2)
+
+    slope1 = numerator1 / denominator1
+    slope2 = numerator2 / denominator2
+    slope3 = numerator3 / denominator3
+
+    intercept = mean_y1 - (slope1 * mean_x1 + slope2 * mean_x2 + slope3 * mean_x3)
+
+    return slope1 * x1 + slope2 * x2 + slope3 * x3 + intercept
+
+xarea = int(input("Enter the area of the house in square feet: "))
+xbedrooms = int(input("Enter the number of bedrooms: "))    
+xbathrooms = int(input("Enter the number of bathrooms: "))
+print("Predicted price:", model1(xarea, xbedrooms, xbathrooms))
